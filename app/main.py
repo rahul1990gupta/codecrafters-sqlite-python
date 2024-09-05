2d85089c0689749c0debd96eb0fd37e0c0cd982e
Rahul Gupta <rahul1990gupta@gmail.com>
2024-09-05 13:04:00 +0530

    adds table count

--- app/main.py
+++ app/main.py
@@ -15,6 +15,12 @@ if command == ".dbinfo":
         # Uncomment this to pass the first stage
         database_file.seek(16)  # Skip the first 16 bytes of the header
         page_size = int.from_bytes(database_file.read(2), byteorder="big")
+        
+        database_file.seek(103) # Skip database header
+        number_of_tables = int.from_bytes(database_file.read(2), byteorder="big")
+
         print(f"database page size: {page_size}")
+        print(f"number of tables: {number_of_tables}")
+         
 else:
     print(f"Invalid command: {command}")
