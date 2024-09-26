bea2df3ca2556d25609ff433ce677a3d987be37f
Rahul Gupta <rahul1990gupta@gmail.com>
2024-09-26 18:52:33 +0530

    adds support for indices

--- app/main.py
+++ app/main.py
@@ -19,11 +19,11 @@ class SQLParser:
         if wi ==-1:
             wi = len(sql)
 
-        select_clause = sql[si:fi]
+        self.select_clause = sql[si:fi]
         from_clause = sql[fi:wi]
         where_clause = sql[wi:]
 
-        self.columns = [col.strip() for col in select_clause[7:].split(",")]
+        self.columns = [col.strip() for col in self.select_clause[7:].split(",")]
         self.table_name = from_clause[5:].strip()
 
         self.has_where = False
@@ -45,6 +45,36 @@ class SQLParser:
             expression = f"\'{value}\'{op}{self.condition[2]}"
             return eval(expression)
         return True
+    
+    def get_condition_value(self):
+        if self.has_where:
+            return self.condition[2]
+
+def get_row_ids(schema_page, table_name, database_file_path, cond_value):
+    index_rootpage = 0
+    for cell in schema_page.cells:
+        if cell.get_value("tbl_name") == table_name and cell.get_value("type") == "index":
+            index_rootpage = cell.get_value("rootpage")
+    
+    with open(database_file_path, "rb") as database_file:
+        database_file.seek((index_rootpage -1 )* 4096)
+        index_rootpage = Page(index_rootpage,
+            database_file.read(4096), 0, 
+                              ["text", "integer"], 
+                              ["country", "rowid"])
+        
+    index_data = index_rootpage.get_data(database_file_path, cond_value)
+    print("cells scanned", len(index_data))
+    filtered_data = [
+        d for d in index_data if d.get("country") == cond_value
+    ]
+    return filtered_data
+
+def index_exists(schema_page, table_name):
+    for cell in schema_page.cells:
+        if cell.get_value("tbl_name") == table_name and cell.get_value("type") == "index":
+            return True
+    return False    
 
 def main(command, database_file_path):
     with open(database_file_path, "rb") as database_file:
@@ -70,22 +100,41 @@ def main(command, database_file_path):
                     if tname != 'sqlite_sequence']
         print(" ".join(tables))
     
-    elif command.lower().startswith("select") and "count" in command:
+    elif command.lower().startswith("select") \
+        and "count" in sql.select_clause:
         with open(database_file_path, "rb") as database_file:
             cell = schema_page.tables[table_name]
-            database_file.seek((cell.get_value("rootpage")-1) * 4096)
+            page_no = cell.get_value("rootpage")
+            database_file.seek((page_no -1) * 4096)
 
-            data_page = Page(database_file.read(4096), 0,  cell.tdtypes, cell.tcnames)
+            data_page = Page(page_no, database_file.read(4096), 0,  cell.tdtypes, cell.tcnames)
             print(data_page.page_header.num_cells)
     elif command.lower().startswith("select"):
         # find rootpage for the table and build data page 
+
+        
+        # assert row_ids == [121311, 2102438, 5729848, 6634629]
+        
+        # {6634629, 2102438, 121311}
+
+        data = []
         with open(database_file_path, "rb") as database_file:
             schema_cell = schema_page.tables[table_name]
-            database_file.seek((schema_cell.get_value("rootpage")-1) * 4096)
+            page_no = schema_cell.get_value("rootpage")
+            database_file.seek((page_no-1) * 4096)
 
-            data_page = Page(database_file.read(4096), 0, schema_cell.tdtypes, schema_cell.tcnames)
+            data_page = Page(page_no, database_file.read(4096), 0, schema_cell.tdtypes, schema_cell.tcnames)
 
-        data = data_page.get_data(database_file_path)
+        if index_exists(schema_page, table_name):
+            cond_value = sql.get_condition_value().replace("'", "")
+            records = get_row_ids(schema_page, table_name, database_file_path, cond_value)
+
+            for record in records:
+                print(record, record.get("rowid"))
+                data+=data_page.get_data(database_file_path, record.get("rowid"))
+            print("data fetched", len(data))
+        else:
+            data = data_page.get_data(database_file_path, None)
 
         for record in data:
             if sql.qualify(record):
--- app/page.py
+++ app/page.py
@@ -23,17 +23,19 @@ class PageHeader:
             self.is_index = True
         else:
             self.is_index = False
-    
+
+        self.cell_content_offset = int.from_bytes(hbytes[5:7], "big")
         self.num_cells = int.from_bytes(hbytes[3:5], "big")
 
 
-
 class Page:
-    def __init__(self, pbytes, offset, dtypes, cnames):
+    def __init__(self, page_no, pbytes, offset, dtypes, cnames):
 
         self.page_header = PageHeader(pbytes[offset:offset + 12])
+        self.page_no = page_no
         if self.page_header.is_interior:
-            self.header_size = 12 
+            self.header_size = 12
+            self.right_most_page_no = int.from_bytes(pbytes[offset+8: offset+12])
         else:
             self.header_size = 8
         self.dtypes = dtypes
@@ -41,9 +43,9 @@ class Page:
     
         self.cell_ptrs =[] 
         self.cells = []
-        self.children = []
 
         offset = offset + self.header_size
+
         cell_ptr_end = offset + 2 * self.page_header.num_cells
         for i in range(offset, cell_ptr_end, 2):
             self.cell_ptrs.append(
@@ -54,15 +56,40 @@ class Page:
                 cell = TableLeafCell(cell_pointer, pbytes, dtypes, cnames)
             elif self.page_header.page_type == PageType.TableInterior.value:
                 cell = TableInteriorCell(cell_pointer, pbytes, dtypes, cnames)
+            elif self.page_header.page_type == PageType.IndexInterior.value:
+                cell = IndexInteriorCell(cell_pointer, pbytes, dtypes, cnames)
+            elif self.page_header.page_type == PageType.IndexLeaf.value:
+                cell = IndexLeafCell(cell_pointer, pbytes, dtypes, cnames)
             else:
                 print("Not supported")
             
             self.cells.append(cell)
     
-    def get_data(self, database_file_path):
+    def _bsearch(self, database_file_path, cond_value):
+        data = []
+        values = [cell.get_index() for cell in self.cells]
+        # print("cells", values)
+        for i, val in enumerate(values):
+            if i-1>=0 and cond_value < values[i-1]:
+                break 
+            if cond_value <= val:
+                data += self.cells[i].get_data(database_file_path, cond_value)
+        # if self.page_header.is_interior:
+        #     with open(database_file_path, "rb") as db:
+        #         db.seek((self.right_most_page_no - 1) * 4096)
+        #         self.right_most_page = Page(
+        #             self.right_most_page_no, db.read(4096), 0, self.dtypes, self.cnames
+        #         )
+        #     data.extend(self.right_most_page._bsearch(database_file_path, cond_value))
+        return data
+    
+    def get_data(self, database_file_path, cond_value):
+        # print("getting data..", self.page_header.page_type)
+        if cond_value:
+            return self._bsearch(database_file_path, cond_value)
         data = []
         for cell in self.cells:
-            cell_data = cell.get_data(database_file_path)
+            cell_data = cell.get_data(database_file_path, cond_value)
             data += cell_data
 
         return data
@@ -72,32 +99,41 @@ class SchemaPage(Page):
         dtypes = ["text", "text", "text", "integer", "text"]
         cnames = ["type", "name", "tbl_name", "rootpage", "sql"]
         
-        super().__init__(pbytes, offset, dtypes, cnames)
+        super().__init__(1, pbytes, offset, dtypes, cnames)
 
         self.tables = {}
         for cell in self.cells:
-            self.tables.update({
-                cell.get_value("tbl_name"): cell
-            })
-        
+            if cell.get_value("type") == "table":
+                self.tables.update({
+                    cell.get_value("tbl_name"): cell 
+                })
+            
 ###################################################################################################
+
 
 def parse_payload(record, dtypes):
     dvalues = []
     header_offset, num_bytes_header = parse_varint(record)
     value_offset = num_bytes_header
     for dtype in dtypes:
-        
         if dtype == "text":
             ix, dtype_serial_type = parse_varint(record[header_offset:])
-            dtype_size = (dtype_serial_type -13)/2
+            if dtype_serial_type ==0:
+                dtype_size = 0
+            else: 
+                dtype_size = (dtype_serial_type -13)/2
         elif dtype == "integer":
             ix, dtype_serial_type = parse_varint(record[header_offset:])
             dtype_size = dtype_serial_type
-
+    
         start = int(value_offset)
         end = start + int(dtype_size)
         dvalue = record[start:end]
+        if dtype == "integer":
+            dvalue = int.from_bytes(dvalue, "big")
+        else:
+            dvalue = dvalue.decode()
+
         dvalues.append(dvalue)
 
         # update offsets 
@@ -126,7 +162,9 @@ class TableLeafCell:
         self.cnames = cnames
         self.dtypes = dtypes
         
-        if self.cnames[-1] == "sql" and self.get_value("tbl_name") != "sqlite_sequence":
+        if self.cnames[-1] == "sql" and \
+            self.get_value("tbl_name") != "sqlite_sequence"\
+                and self.get_value("type") == "table":
             self.process_sql(self.get_value("sql"))
 
 
@@ -137,46 +175,126 @@ class TableLeafCell:
                 return self.row_id
             elif col == name:
                 value = self.dvalues[index]
-                if self.dtypes[index] == "integer":
-                    value = int.from_bytes(value, "big")
-                else: 
-                    value = value.decode()
                 return value
-        
     
     def process_sql(self, sql):
         columns = parse_sql(sql)
-        self.tcnames = [col.split(" ")[0] for col in columns]
-        self.tdtypes = [col.split(" ")[1] for col in columns]
+        self.tcnames = []
+        self.tdtypes = []
+        for col in columns:
+            if "\"" in col:
+                dtype = col.split(" ")[-1]
+                cname = col[: len(dtype) -1]
+            else:
+                dtype = col.split(" ")[1]
+                cname = col.split(" ")[0]
+            self.tcnames.append(cname)
+            self.tdtypes.append(dtype)
     
-    def get_data(self, database_file_path):
+    def get_data(self, database_file_path, cond_value):
+        # print("getting data...", TableLeafCell)
         data_dict = {
-            col: self.dvalues[i].decode()
+            col: self.dvalues[i]
             for i, col in enumerate(self.cnames)
         }
         data_dict.update({"id": self.row_id})
         return [data_dict]
+    
+    def get_index(self):
+        return self.row_id
+
 
 class TableInteriorCell:
     def __init__(self, cell_pointer, pbytes, dtypes, cnames):
         self.child_page_no = int.from_bytes(pbytes[cell_pointer: cell_pointer+4], "big")
-        ix, rowid = parse_varint(pbytes[cell_pointer + 4: cell_pointer + 13])
+        ix, self.rowid = parse_varint(pbytes[cell_pointer + 4: cell_pointer + 13])
         self.dtypes = dtypes 
-        self.cnames = self.cnames 
+        self.cnames = cnames 
 
-    def get_data(self, database_file_path):
+    def get_data(self, database_file_path, cond_value):
+        # print("getting data...", TableInteriorCell)
         with open(database_file_path, "rb") as f:
             f.seek((self.child_page_no - 1) * 4096)
-            self.child_page = Page(f.read(4096), 0, self.dtypes, self.cnames)
+            self.child_page = Page(self.child_page_no, f.read(4096), 0, self.dtypes, self.cnames)
         
-        return self.child_page.get_data(database_file_path)
+        return self.child_page.get_data(database_file_path, cond_value)
+    
+    def get_index(self):
+        return self.rowid
 
+
+"""
+A varint which is the total number of bytes of key payload, including any overflow
+The initial portion of the payload that does not spill to overflow pages.
+A 4-byte big-endian integer page number for the first page of the overflow page list - omitted if all payload fits on the b-tree page.
+"""
 class IndexLeafCell:
-    def __init__(self):
-        pass 
+    def __init__(self, cell_pointer, pbytes, dtypes, cnames):
+        ix, payload_size = parse_varint(pbytes[cell_pointer:])
+        if payload_size > 4000:
+            print("Big payload. Need to process overflow pages")
+        
+        # parse_payload(record, dtypes)
+        start = cell_pointer + ix 
+        end = start + payload_size
+        dvalues = parse_payload(
+            pbytes[start:end], 
+            dtypes
+        )
+        self.dvalues = dvalues
+        self.cnames = cnames 
+    
 
+    def get_data(self, database_file_path, cond_value):
+        # print("getting data...", IndexLeafCell)
+        # print(self.dvalues)
+        record = [{
+            col: self.dvalues[i]
+            for i, col in enumerate(self.cnames)
+        }]
+        return record 
+
+    def get_index(self):
+        return self.dvalues[0]
+
+"""
+A 4-byte big-endian page number which is the left child pointer.
+
+A varint which is the total number of bytes of key payload, including any overflow
+
+The initial portion of the payload that does not spill to overflow pages.
+
+A 4-byte big-endian integer page number for the first page of the overflow page list - omitted if all payload fits on the b-tree page.
+"""
 class IndexInteriorCell:
-    def __init__(self):
-        pass 
+    def __init__(self, cell_pointer, pbytes, dtypes, cnames):
+        self.child_page_no = int.from_bytes(pbytes[cell_pointer:cell_pointer+4], "big")
 
+        ix, payload_size = parse_varint(pbytes[cell_pointer+4: cell_pointer+8])
 
+        if payload_size > 4000:
+            print("Big payload. Need to process overflow pages")
+
+        start = cell_pointer + 4 + ix 
+        end = start + payload_size
+        dvalues = parse_payload(
+            pbytes[start:end], 
+            dtypes
+        )
+        self.dvalues = dvalues 
+        self.dtypes = dtypes
+        self.cnames = cnames 
+
+    def get_data(self, database_file_path, cond_value):
+        # print("getting data...", IndexInteriorCell, self.get_index())
+
+        with open(database_file_path, "rb") as f:
+            f.seek((self.child_page_no - 1) * 4096)
+            self.child_page = Page(self.child_page_no,
+                f.read(4096), 0, self.dtypes, self.cnames)
+        
+        return self.child_page.get_data(database_file_path, cond_value)
+
+    def get_index(self):
+        return self.dvalues[0]
+      
